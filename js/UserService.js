aaf615e008628af10b3326bfd0115cb48b4702d8
George Heeres <gheeres@gmail.com>
2024-10-03 14:30:04 -0500

    Initial commit. Sample JavaScript application

---
+++ js/UserService.js
@@ -0,0 +1,75 @@
+import { getRandomInt } from './utilities.js';
+
+const baseUrl = 'https://randomuser.me/api'
+export default class UserService {
+  constructor(url) {
+    this.url = url | baseUrl;
+  }  
+
+  /**
+   * Converts a JSON response into a user object.
+   * @param {String} json The JSON response.
+   * @returns {Object} The user object.
+   */
+  #toUser(json) {
+    return {
+      id: json.id.value,
+      lastName : json.name?.last,
+      firstName : json.name?.first,
+      email: json.email,
+      photo: json.picture.medium,
+      title: this.#getRandomTitle(json),
+      socialMedia: this.#getRandomSocialMedia(json),
+      followers: [ ],
+    };
+  }
+
+  /**
+   * Randomly generate or retrieve a job title for a user.
+   * @param {String} json The JSON response.
+   */
+  #getRandomTitle(json) {
+    const titles = [ 
+      'Marketing Coordinator',
+      'Medical Assistant',
+      'Web Designer',
+      'Dog Trainer',
+      'President of Sales',
+      'Nursing Assistant',
+      'Project Manager',
+      'Librarian',
+      'Project Manager',
+      'Account Executive',
+    ];
+    return titles[getRandomInt(titles.length)];
+  }
+
+  /**
+   * Randomly generated social media links.
+   * @param {String} json The JSON response.
+   */
+  #getRandomSocialMedia(json) {
+    return [ 
+      getRandomInt(2) === 1 ? { type: 'facebook', value: `${ json.username }` } : null,
+      getRandomInt(2) === 1 ? { type: 'twitter', value: `#${ json.username }` } : null,
+      getRandomInt(2) === 1 ? { type: 'skype', value: `${ json.phone }` } : null,
+    ].filter(f => f != null);
+  }
+
+  /**
+   * Retrieves a random user from the external endpoint.
+   * @returns {Object} The user information.
+   */
+  async get() {
+    const url = `${ baseUrl }`;
+    console.debug(`Requesting data from url: ${ url }`);
+    const response = await fetch(url);
+    if (response.status === 200) {
+      const json = await response.json();
+      return this.#toUser(json.results[0]);  
+    }
+
+    console.error(`Failed to retrieve data from url: ${ url }. Status(${ response.status }): ${ response.text }`);
+    return null;
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,57 @@
+import Profile from './Profile.js';
+import UserService from './UserService.js';
+
+const service = new UserService();
+
+const profiles = document.querySelector("#profiles");
+const addUserButton = document.querySelector('.control-add-user');
+addUserButton.addEventListener('click', async (e) => {
+  const user = await service.get();
+  if (user) {
+    const profile = Profile(user);
+    profiles.append(profile);
+  }
+});
+
+profiles.addEventListener('click', (e) => {
+  const element = e.target.closest('.profile');
+  // Follow
+  if (e.target.classList.contains('js-profile-follow')) {
+    profiles.dispatchEvent(new CustomEvent('follow', { 
+      detail: {
+        originalEvent: e,
+        user: JSON.parse(element?.dataset?.user || '{}'),
+        element: element,
+      }
+    }));
+  }
+  // Delete
+  if (e.target.classList.contains('js-profile-delete')) {
+    profiles.dispatchEvent(new CustomEvent('delete', { 
+      detail: {
+        originalEvent: e,
+        user: JSON.parse(element?.dataset?.user || '{}'),
+        element: element,
+      }
+    }));
+  }
+});
+
+profiles.addEventListener('follow', (e) => {
+  const existingUser = e.detail?.user;
+  if (existingUser) {
+    const updatedUser = { ...existingUser, followers: [ ...existingUser.followers || [], 'another' ] };
+    const updatedProfile = Profile(updatedUser);
+    if (updatedProfile) {
+      const element = e.detail?.element;
+      element?.replaceWith(updatedProfile);
+    }
+  }
+});
+
+profiles.addEventListener('delete', (e) => {
+  const element = e.detail?.element;
+  if (element) {
+    element?.remove();
+  }
+});
